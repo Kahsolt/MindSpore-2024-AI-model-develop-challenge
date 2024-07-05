61f90793ae55ef834810a95975c2ef5ea5e03c39
Kahsolt <kahsolt@qq.com>
2024-07-05 13:22:38 +0800

    refactor
    fix regex bug
    add thesis
    remove qa test data

---
+++ P2#模型微调赛题/stage1/thesis/download.cmd
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+REM Case-Based or Rule-Based: How Do Transformers Do the Math?
+REM brief view: https://mp.weixin.qq.com/s/aVRiGW3xU_LpvxZzjDpwzQ
+wget -nc https://arxiv.org/pdf/2402.17709.pdf
